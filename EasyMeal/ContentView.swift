f73f556347a5e5127bd6cf53b07fdea3aa75aed4
Alex Strugacz <90818590+Nectar0@users.noreply.github.com>
2023-02-01 17:00:19 -0600

    Initial Commit

---
+++ EasyMeal/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  EasyMeal
+//
+//  Created by Alex Strugacz on 2/1/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ EasyMeal/EasyMealApp.swift
@@ -0,0 +1,17 @@
+//
+//  EasyMealApp.swift
+//  EasyMeal
+//
+//  Created by Alex Strugacz on 2/1/23.
+//
+
+import SwiftUI
+
+@main
+struct EasyMealApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
