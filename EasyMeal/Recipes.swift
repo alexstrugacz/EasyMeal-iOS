1f358118e412fd4df151e99686cc181b8d3405fd
Alexander Masztak <itzalqxander@gmail.com>
2023-02-10 20:16:20 -0600

    recipe page

--- EasyMeal/Recipes.swift
+++ EasyMeal/Recipes.swift
@@ -1,67 +1,133 @@
-//
-//  Recipes.swift
-//  EasyMeal
-//
-//  Created by Alexander Masztak on 2/10/23.
-//
-
 import SwiftUI
 
 struct Recipes: View {
     @State var showView = false
-
+    
     var body: some View {
-            VStack {
-                Text("Recipes")
-                    .font(.largeTitle)
-                    .bold()
-                    .multilineTextAlignment(.center)
-                
+        ScrollView {
+            
+            Text("Recipes")
+                .font(.largeTitle)
+                .bold()
+                .multilineTextAlignment(.center)
+            
             VStack(spacing: 20) {
-                            ScrollView(.horizontal) {
-                                HStack(spacing: 20) {
-                                    ForEach(0..<10) { index in
-                                        Button(action: {
-                                            print("Button tapped: \(index)")
-                                        }) {
-                                            Text("Button \(index)")
-                                        }
-                                        .frame(width: 60, height: 60)
-                                        .background(Color.yellow)
-                                        .cornerRadius(30)
+                Text("Recently Added")
+                    .bold()
+                    .multilineTextAlignment(.leading)
+                    .offset(x: -110, y: 10)
+                    .font(.system(size: 23))
+                
+                ScrollView(.horizontal) {
+                    HStack(spacing: 20) {
+                        ForEach(0..<10) { index in
+                            VStack(spacing: 8) {
+                                VStack {
+                                    Button(action: {
+                                        print("Button tapped: \(index)")
+                                    }) {
+                                        Text("Button \(index)")
+                                            .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity)
                                     }
+                                    .frame(width: 160, height: 160)
+                                    .background(Color.yellow)
+                                    .cornerRadius(10)
                                 }
-                            }
-                            ScrollView(.horizontal) {
-                                HStack(spacing: 20) {
-                                    ForEach(10..<20) { index in
-                                        Button(action: {
-                                            print("Button tapped: \(index)")
-                                        }) {
-                                            Text("Button \(index)")
-                                        }
-                                        .frame(width: 60, height: 60)
-                                        .background(Color.yellow)
-                                        .cornerRadius(30)
-                                    }
-                                }
-                            }
-                            ScrollView(.horizontal) {
-                                HStack(spacing: 20) {
-                                    ForEach(20..<30) { index in
-                                        Button(action: {
-                                            print("Button tapped: \(index)")
-                                        }) {
-                                            Text("Button \(index)")
-                                        }
-                                        .frame(width: 60, height: 60)
-                                        .background(Color.yellow)
-                                        .cornerRadius(30)
+                                
+                                VStack(alignment: .leading) {
+                                    Text("Title \(index)")
+                                        .font(.headline)
+                                    HStack {
+                                        Text("Subtitle A \(index)")
+                                            .font(.subheadline)
+                                        Text("Subtitle B \(index)")
+                                            .font(.subheadline)
                                     }
                                 }
                             }
                         }
+                    }
+                }
             }
+            
+            VStack(spacing: 20) {
+                Text("Recently Added")
+                    .bold()
+                    .multilineTextAlignment(.leading)
+                    .offset(x: -110, y: 10)
+                    .font(.system(size: 23))
+                
+                ScrollView(.horizontal) {
+                    HStack(spacing: 20) {
+                        ForEach(0..<10) { index in
+                            VStack(spacing: 8) {
+                                VStack {
+                                    Button(action: {
+                                        print("Button tapped: \(index)")
+                                    }) {
+                                        Text("Button \(index)")
+                                            .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity)
+                                    }
+                                    .frame(width: 160, height: 160)
+                                    .background(Color.yellow)
+                                    .cornerRadius(10)
+                                }
+                                
+                                VStack(alignment: .leading) {
+                                    Text("Title \(index)")
+                                        .font(.headline)
+                                    HStack {
+                                        Text("Subtitle A \(index)")
+                                            .font(.subheadline)
+                                        Text("Subtitle B \(index)")
+                                            .font(.subheadline)
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            
+            VStack(spacing: 20) {
+                Text("Recently Added")
+                    .bold()
+                    .multilineTextAlignment(.leading)
+                    .offset(x: -110, y: 10)
+                    .font(.system(size: 23))
+                
+                ScrollView(.horizontal) {
+                    HStack(spacing: 20) {
+                        ForEach(0..<10) { index in
+                            VStack(spacing: 8) {
+                                VStack {
+                                    Button(action: {
+                                        print("Button tapped: \(index)")
+                                    }) {
+                                        Text("Button \(index)")
+                                            .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity)
+                                    }
+                                    .frame(width: 160, height: 160)
+                                    .background(Color.yellow)
+                                    .cornerRadius(10)
+                                }
+                                
+                                VStack(alignment: .leading) {
+                                    Text("Title \(index)")
+                                        .font(.headline)
+                                    HStack {
+                                        Text("Subtitle A \(index)")
+                                            .font(.subheadline)
+                                        Text("Subtitle B \(index)")
+                                            .font(.subheadline)
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            
+        }
     }
 }
-
