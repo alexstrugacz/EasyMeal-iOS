f203c04b8f9c6724ad122e2a876f29a6ca918cc9
Alexander Masztak <itzalqxander@gmail.com>
2023-02-11 19:05:50 -0600

    tab bar

---
+++ EasyMeal/TabBar.swift
@@ -0,0 +1,60 @@
+
+
+import SwiftUI
+
+enum Tab: String, CaseIterable {
+    case refrigerator
+    case cooktop
+    case cart
+    case person
+}
+
+struct CustomTabBar: View {
+    @Binding var selectedTab: Tab
+    private var fillImage: String {
+        selectedTab.rawValue + ".fill"
+    }
+    private var tabColor: Color {
+        switch selectedTab {
+        case .refrigerator:
+            return .white
+        case .cooktop:
+            return .white
+        case .cart:
+            return .white
+        case .person:
+            return .white
+        }
+    }
+    
+    
+    var body: some View {
+        VStack {
+            HStack {
+                ForEach(Tab.allCases, id: \.rawValue) { tab in
+                    Spacer()
+                    Image(systemName: selectedTab == tab ? fillImage : tab.rawValue)
+                        .scaleEffect(tab == selectedTab ? 1.25 : 1.0)
+                        .foregroundColor(tab == selectedTab ? tabColor : .white)
+                        .font(.system(size: 20))
+                        .onTapGesture {
+                            withAnimation(.easeInOut(duration: 0.1)) {
+                                selectedTab = tab
+                            }
+                        }
+                    Spacer()
+                }
+            }
+            .frame(width: nil, height: 65)
+            .background(custGreen)
+            .cornerRadius(50)
+            .padding()
+        }
+    }
+}
+
+struct CustomTabBar_Previews: PreviewProvider {
+    static var previews: some View {
+        CustomTabBar(selectedTab: .constant(.refrigerator))
+    }
+}
