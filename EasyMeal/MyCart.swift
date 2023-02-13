ac27e9eb3848adb2e57b41d2701ba33555154fd1
Alexander Masztak <itzalqxander@gmail.com>
2023-02-12 23:20:35 -0600

    shopping list

--- EasyMeal/MyCart.swift
+++ EasyMeal/MyCart.swift
@@ -1,24 +1,54 @@
-//
-//  MyCart.swift
-//  EasyMeal
-//
-//  Created by Alexander Masztak on 2/10/23.
-//
-
 import SwiftUI
 
 struct MyCart: View {
-    @State var showView = false
-
+    @State private var items = [
+        ShoppingItem(name: "Apples", isChecked: false),
+        ShoppingItem(name: "Bananas", isChecked: false),
+        ShoppingItem(name: "Oranges", isChecked: false),
+        ShoppingItem(name: "Pears", isChecked: false)
+    ]
+    
     var body: some View {
-            VStack {
-                Text("MyCart")
-                    .font(.largeTitle)
-                    .bold()
-                    .multilineTextAlignment(.center)
-                
-                
+        NavigationView {
+            List {
+                ForEach(items, id: \.id) { item in
+                    Section() {
+                        HStack {
+                            Button(action: {
+                                if let index = self.items.firstIndex(where: { $0.id == item.id }) {
+                                    self.items[index].isChecked.toggle()
+                                }
+                            }) {
+                                if item.isChecked {
+                                    Image(systemName: "checkmark.square")
+                                        .foregroundColor(.green)
+                                } else {
+                                    Image(systemName: "square")
+                                        .foregroundColor(.black)
+                                }
+                            }
+                            Text(item.name)
+                        }
+                        .padding(.vertical, 10)
+                    }
+                }
             }
+            .listStyle(GroupedListStyle())
+            
+            .navigationBarTitle("Shopping List")
+        }
+    }
+}
+
+struct ShoppingItem: Identifiable, Hashable {
+    let id = UUID()
+    var name: String
+    var isChecked: Bool
+}
+
+struct MyCart_Previews: PreviewProvider {
+    static var previews: some View {
+        MyCart()
     }
 }
 
