b2f52008316d2d464ddb100329987d5dc2c63287
Alexander Masztak <itzalqxander@gmail.com>
2023-02-14 23:18:35 -0600

    my cart. shadows on category text need removing

--- EasyMeal/MyCart.swift
+++ EasyMeal/MyCart.swift
@@ -2,69 +2,79 @@ import SwiftUI
 
 struct MyCart: View {
     @State private var items = [
-        ShoppingItem(name: "Chicken", isChecked: false, size: "1 C."),
-        ShoppingItem(name: "Carrots", isChecked: false, size: "0.5 lb."),
-        ShoppingItem(name: "Bread Crumbs", isChecked: false, size: "Pinch"),
-        ShoppingItem(name: "Peppers", isChecked: false, size: "2")
+        ShoppingItem(name: "Chicken", isChecked: false, size: "1 C.", category: "Meat"),
+        ShoppingItem(name: "Carrots", isChecked: false, size: "0.5 lb.", category: "Vegetables"),
+        ShoppingItem(name: "Bread Crumbs", isChecked: false, size: "Pinch", category: "Pantry"),
+        ShoppingItem(name: "Peppers", isChecked: false, size: "2", category: "Vegetables")
     ]
+    
+    var groupedItems: [String: [ShoppingItem]] {
+        Dictionary(grouping: items, by: { $0.category })
+    }
 
     var body: some View {
         VStack {
             HStack {
-                        Text("My Cart")
-                            .font(.title)
-                            .bold()
-                            .foregroundColor(.black)
-                            //.padding(.leading, 20)
-                        
-                        //Spacer()
-                    }
-
+                Text("My Cart")
+                    .font(.title)
+                    .bold()
+                    .foregroundColor(.black)
+            }
+            
             ScrollView {
-                ForEach(items) { item in
-                    ZStack {
-                        RoundedRectangle(cornerRadius: 10)
-                            .frame(height: 60)
-                            .foregroundColor(custGray)
-                            .onTapGesture {
-                                if let index = self.items.firstIndex(where: { $0.id == item.id }) {
-                                    self.items[index].isChecked.toggle()
+                ForEach(groupedItems.keys.sorted(), id: \.self) { category in
+                    VStack(alignment: .leading) {
+                        Text(category)
+                            .font(.subheadline)
+                            .padding(.leading, 20)
+                        ForEach(groupedItems[category]!, id: \.id) { item in
+                            ZStack {
+                                RoundedRectangle(cornerRadius: 10)
+                                    .frame(height: 60)
+                                    .foregroundColor(custGray)
+                                    .onTapGesture {
+                                        if let index = self.items.firstIndex(where: { $0.id == item.id }) {
+                                            self.items[index].isChecked.toggle()
+                                        }
+                                    }
+                                
+                                HStack {
+                                    if item.isChecked {
+                                        Image(systemName: "checkmark.square")
+                                            .foregroundColor(.green)
+                                            .padding(.leading, 30) // add leading padding
+                                    } else {
+                                        Image(systemName: "square")
+                                            .foregroundColor(.black)
+                                            .padding(.leading, 30) // add leading padding
+                                    }
+                                    
+                                    Text(item.name)
+                                        .font(.subheadline)
+                                    
+                                    Spacer()
+                                    
+                                    Text(item.size)
+                                        .font(.subheadline)
+                                        .foregroundColor(.gray)
+                                        .padding(.trailing, 10)
                                 }
+                                .padding(.vertical, 10)
                             }
-                        
-                        HStack {
-                            if item.isChecked {
-                                Image(systemName: "checkmark.square")
-                                    .foregroundColor(.green)
-                                    .padding(.leading, 30) // add leading padding
-                            } else {
-                                Image(systemName: "square")
-                                    .foregroundColor(.black)
-                                    .padding(.leading, 30) // add leading padding
-                            }
-                            
-                            Text(item.name)
-                                .font(.subheadline)
-                            
-                            Spacer()
-                            
-                            Text(item.size)
-                                .font(.subheadline)
-                                .foregroundColor(.gray)
-                                .padding(.trailing, 10)
+                            .cornerRadius(10)
                         }
-                        .padding(.vertical, 10)
-                    }
-                    .background(Color.white)
-                    .cornerRadius(10)
 
+                    }
+                    
                 }
                 
             }
-            .shadow(radius: 3)
+            
             .padding(.horizontal, 20)
             Spacer()
         }
+        .shadow(radius: 3)
+
     }
 
     struct ShoppingItem: Identifiable, Hashable {
@@ -72,6 +82,7 @@ struct MyCart: View {
         var name: String
         var isChecked: Bool
         var size: String
+        var category: String
     }
 
     struct MyCart_Previews: PreviewProvider {
