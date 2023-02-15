52caf2c559e7f3203ce635f8caacee76787964b3
Alexander Masztak <itzalqxander@gmail.com>
2023-02-14 22:39:31 -0600

    mycart

--- EasyMeal/MyCart.swift
+++ EasyMeal/MyCart.swift
@@ -7,65 +7,77 @@ struct MyCart: View {
         ShoppingItem(name: "Bread Crumbs", isChecked: false, size: "Pinch"),
         ShoppingItem(name: "Peppers", isChecked: false, size: "2")
     ]
-    
+
     var body: some View {
         VStack {
-            Text("MyCart")
-                .font(.title)
-                .foregroundColor(.black)
-                .padding()
-            
+            HStack {
+                        Text("My Cart")
+                            .font(.title)
+                            .bold()
+                            .foregroundColor(.black)
+                            //.padding(.leading, 20)
+                        
+                        //Spacer()
+                    }
+
             ScrollView {
-                ForEach(items, id: \.id) { item in
+                ForEach(items) { item in
                     ZStack {
                         RoundedRectangle(cornerRadius: 10)
                             .frame(height: 60)
                             .foregroundColor(custGray)
-
-                        
-                        HStack {
-                            Button(action: {
+                            .onTapGesture {
                                 if let index = self.items.firstIndex(where: { $0.id == item.id }) {
                                     self.items[index].isChecked.toggle()
                                 }
-                            }) {
-                                if item.isChecked {
-                                    Image(systemName: "checkmark.square")
-                                        .foregroundColor(.green)
-                                } else {
-                                    Image(systemName: "square")
-                                        .foregroundColor(.black)
-                                }
                             }
-                            .padding(.leading, 10)
+                        
+                        HStack {
+                            if item.isChecked {
+                                Image(systemName: "checkmark.square")
+                                    .foregroundColor(.green)
+                                    .padding(.leading, 30) // add leading padding
+                            } else {
+                                Image(systemName: "square")
+                                    .foregroundColor(.black)
+                                    .padding(.leading, 30) // add leading padding
+                            }
+                            
                             Text(item.name)
                                 .font(.subheadline)
                             
                             Spacer()
                             
                             Text(item.size)
-                                    .font(.subheadline)
-                                    .foregroundColor(.gray)
-                                    .padding(.trailing, 10)
+                                .font(.subheadline)
+                                .foregroundColor(.gray)
+                                .padding(.trailing, 10)
                         }
                         .padding(.vertical, 10)
                     }
+                    .background(Color.white)
+                    .cornerRadius(10)
+
                 }
-            }.padding(.horizontal, 20)
-                .shadow(radius: 5)
+                
+            }
+            .shadow(radius: 3)
+            .padding(.horizontal, 20)
+            Spacer()
         }
     }
-    
+
     struct ShoppingItem: Identifiable, Hashable {
         let id = UUID()
         var name: String
         var isChecked: Bool
         var size: String
     }
-    
+
     struct MyCart_Previews: PreviewProvider {
         static var previews: some View {
             MyCart()
         }
     }
 }
+
