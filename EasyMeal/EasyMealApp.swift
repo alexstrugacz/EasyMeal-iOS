79b4b71f663bda1eab17bff91f23da2647aed733
Alex Strugacz <90818590+Nectar0@users.noreply.github.com>
2023-05-16 20:07:15 -0500

    Add firebase SDK + dependancies

--- EasyMeal/EasyMealApp.swift
+++ EasyMeal/EasyMealApp.swift
@@ -5,7 +5,19 @@
 //  Created by Alex Strugacz on 2/1/23.
 //
 
+
 import SwiftUI
+import FirebaseCore
+
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+
+    return true
+  }
+}
 
 @main
 struct EasyMealApp: App {
