78a15590013cabccc06bd523c2814058cb11336d
Alex Strugacz <90818590+Nectar0@users.noreply.github.com>
2023-05-16 19:55:31 -0500

    sign up UI complete

--- EasyMeal/Logins/SignUp.swift
+++ EasyMeal/Logins/SignUp.swift
@@ -13,104 +13,107 @@ struct SignUp: View {
     @State private var password: String = ""
     
     var body: some View {
-        VStack {
-            ZStack(alignment: .topLeading) {
-            
-
-            Color.clear
-            VStack(alignment: .leading) {
-
-                Text("EasyMeal")
-                    .multilineTextAlignment(.center)
-                    .foregroundColor(custGreen)
-                    .font(.system(size: 60)).bold()
-                
-            }
-            .offset(x: 40, y: 75)
-        }
-            
-            ZStack(alignment: .bottom) {
-                HStack {
-                    HStack {
-                        Image("email")
-                            .resizable()
-                            .frame(width: 20, height: 20)
-                            .padding(.leading, 10)
-                        TextField("Email", text: $email)
-                            .lineLimit(1)
-                            .padding(.leading, 15)
-                            .background(Color.clear)
-                    }
-                    .frame(width: 280, height: 50)
-                    .background(RoundedRectangle(cornerRadius: 5).fill(Color.white))
-                }
-                
-                RoundedRectangle(cornerRadius: 5)
-                    .stroke(lineWidth: 0.5)
-                    .foregroundColor(.black)
-                    .frame(width: 300, height: 1)
-                    .padding(.bottom, 10)
-            }
-            .padding(.top, 20)
-            .offset(y: -250)
-
-
-
-
+        ZStack {
+            Color.white
+            VStack() {
+                HStack() {
+                    Image("logo")
+                        .resizable()
+                        .frame(width: 50, height: 83)
+                        .aspectRatio(contentMode: .fill)
                         
-            ZStack(alignment: .bottom) {
-                HStack {
-                    HStack {
-                        Image("password")
-                            .resizable()
-                            .frame(width: 20, height: 20)
-                            .padding(.leading, 10)
-                        SecureField("Password", text: $password)
-                            .lineLimit(1)
-                            .padding(.leading, 15)
-                            .background(Color.clear)
-                    }
-                    .frame(width: 280, height: 50)
-                    .background(RoundedRectangle(cornerRadius: 5).fill(Color.white))
-                }
+                        .shadow(radius: 3)
+                    Text("EasyMeal")
+                        .bold()
+                        .foregroundColor(custGreen)
+                        .font(.system(size: 45))
+                }.frame(maxWidth: .infinity, alignment: .leading).offset(x: 35)
                 
-                RoundedRectangle(cornerRadius: 5)
-                    .stroke(lineWidth: 0.5)
-                    .foregroundColor(.black)
-                    .frame(width: 300, height: 1)
-                    .padding(.bottom, 10)
-            }
-            .padding(.top, 20)
-            .offset(y: -250)
-
-
-
-
-                            
-
-            
-            Button(action: {
-                //code
-                print("toPantry")
-                                    DispatchQueue.main.asyncAfter(deadline: .now()) {
-                                        self.showView = true
-                                    }
-            }) {
-                Text("Create")
-                    .foregroundColor(.white)
-                    .padding(.horizontal, 130)
-                    .padding(.vertical, 16)
-                    .background(LinearGradient(gradient: Gradient(colors: [custGreen, custGreen]), startPoint: .leading, endPoint: .trailing))
+                Rectangle()
+                    .foregroundColor(Color(hex: "efefef"))
+                    .frame(width: 350, height: 50)
                     .cornerRadius(10)
-                    .shadow(color: custShadow, radius: 1, x: 1, y: 1)
-            }
-            .offset(y: -175)
-            NavigationLink(destination: MainView(initialTab: .refrigerator, content: {
-                Pantry()
-            }), isActive: $showView) {
-                                EmptyView()
-                            }
-        }
+                    .offset(y: 4)
+                
+                HStack {
+                    Image(systemName: "envelope")
+                        .foregroundColor(Color(hex: "747474"))
+                    TextField("Email", text: $email)
+                        
+                }.offset(x: 50, y: -33)
+                    
+                Rectangle()
+                    .foregroundColor(Color(hex: "efefef"))
+                    .frame(width: 350, height: 50)
+                    .cornerRadius(10)
+                    .offset(y: -8)
+                HStack {
+                    Image(systemName: "lock")
+                        .foregroundColor(Color(hex: "747474"))
+                    
+                    HStack {
+                        SecureField("Password", text: $password)
+                        Image(systemName: "eye")
+                            .foregroundColor(Color(hex: "747474"))
+                    }
+                }.offset(x: 50, y: -46)
+                
+                Rectangle()
+                    .foregroundColor(Color(hex: "efefef"))
+                    .frame(width: 350, height: 50)
+                    .cornerRadius(10)
+                    .offset(y: -20)
+                HStack {
+                    Image(systemName: "lock")
+                        .foregroundColor(Color(hex: "747474"))
+                    
+                    HStack {
+                        SecureField("Confirm Password", text: $password)
+                        Image(systemName: "eye")
+                            .foregroundColor(Color(hex: "747474"))
+                    }
+                }.offset(x: 50, y: -58)
+                
+                HStack {
+                    Button(action: {
+                        // Add your button action here
+                    }) {
+                        Text("Sign Up")
+                            .frame(width: 234, height: 50)
+                            .cornerRadius(20)
+                            .foregroundColor(Color.white)
+                            .bold()
+                    }
+                    .frame(width: 350, height: 50)
+                    .background(custGreen)
+                    .cornerRadius(10)
+                }.frame(width: 350, height: 50).offset(y: -15)
+                
+                Button(action: {
+                    // Add your button action here
+                }) {
+                    Image(systemName: "apple.logo")
+                        .foregroundColor(Color.white)
+                    Text("Sign Up with Apple")
+                        .frame(height: 50)
+                        .cornerRadius(20)
+                        .foregroundColor(Color.white)
+                        .bold()
+                }
+                .frame(width: 350, height: 50)
+                .background(Color.black)
+                .cornerRadius(10)
+                .offset(y: -15)
+                
+                Text("By using EasyMeal, you agree to our Terms of Service")
+                    .bold()
+                    .foregroundColor(Color(hex: "7B7A7A"))
+                    .font(.caption)
+                    .offset(y: -15)
+                
+                
+            }.frame(height: 500, alignment: .top)
+        }.ignoresSafeArea()
     }
 }
 struct SignUp_Previews: PreviewProvider {
