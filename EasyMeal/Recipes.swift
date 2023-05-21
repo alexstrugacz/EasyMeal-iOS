e9add481d731816e5b1c3fda6c472ea36611d24c
Rachel Rafik <tarvotur15@gmail.com>
2023-05-21 14:53:40 -0500

    added filters

--- EasyMeal/Recipes.swift
+++ EasyMeal/Recipes.swift
@@ -2,6 +2,10 @@ import SwiftUI
 
 struct Recipes: View {
     @State var showView = false
+    @State var showHealth = false
+    @State var showMinCal = false
+    @State var showMaxCal = false
+    
     @ObservedObject var recipesViewModel: RecipesViewModel = RecipesViewModel()
     
     var openCart: () -> Void
@@ -12,20 +16,155 @@ struct Recipes: View {
         openCart()
     }
     
+    @State var showFilter = false
+    
+    @State private var score = 0.0
+    @State private var min = 100.0
+    @State private var max = 2000.0
+        
+        let numberFormatter: NumberFormatter = {
+            let num = NumberFormatter()
+            num.maximumFractionDigits = 0
+            return num
+        }()
+    
     var body: some View {
         VStack{
 
-            VStack {
-                Text("Recipes")
-                    .font(.title)
-                    .bold()
-                    .foregroundColor(.black)
-                
-                
-            }
-            .foregroundColor(Color.white.opacity(1))
-            .offset(y:20)
-            .padding(.bottom, 40)
+            HStack {
+                            Button(action: {
+                                showFilter.toggle()
+                            }) {
+                                Image(systemName: "slider.horizontal.3") // This is a built-in SwiftUI filter icon
+                                    .resizable()
+                                    .frame(width: 33, height: 30)
+                                    .foregroundColor(.blue)
+                                    .padding(.leading,10)
+                            }
+                            .padding()
+
+                            Spacer()
+
+                            Text("Recipes")
+                                .font(.title)
+                                .bold()
+                                .foregroundColor(.black)
+                                .offset(x: -40)
+
+                            Spacer()
+                        }
+                        .offset(y: -5)
+                        .foregroundColor(Color.white.opacity(1))
+                        .padding(.bottom, 1)
+            
+                        if showFilter {
+                            HStack {
+                                Button(action: {
+                                    if showHealth == true{
+                                        showHealth = false
+                                    } else {
+                                        showHealth = true
+                                        showMaxCal = false
+                                        showMinCal = false
+                                    }
+                                    }
+                                ) {
+                                    RoundedRectangle(cornerRadius: 10)
+                                        .foregroundColor(custGreen)
+                                        .frame(width: 120, height: 30)
+                                        .overlay(
+                                            Text("Heath Score")
+                                                .foregroundColor(.white)
+                                                .padding(.vertical, 10)
+                                        )
+                                }
+                                
+                                Spacer()
+                                
+                                Button(action: {
+                                    if showMinCal == true{
+                                        showMinCal = false
+                                    } else {
+                                        showHealth = false
+                                        showMaxCal = false
+                                        showMinCal = true
+                                    }
+                                    }
+                                ) {
+                                    RoundedRectangle(cornerRadius: 10)
+                                        .foregroundColor(custMedGreen)
+                                        .frame(width: 120, height: 30)
+                                        .overlay(
+                                            Text("Min Calories")
+                                                .foregroundColor(.white)
+                                                .padding(.vertical, 10)
+                                        )
+                                }
+                                
+                                Spacer()
+                                
+                                
+                                Button(action: {
+                                    if showHealth == true{
+                                        showMaxCal = false
+                                    } else {
+                                        showHealth = false
+                                        showMaxCal = true
+                                        showMinCal = false
+                                    }
+                                    }
+                                ) {
+                                    RoundedRectangle(cornerRadius: 10)
+                                        .foregroundColor(custDarkGreen)
+                                        .frame(width: 120, height: 30)
+                                        .overlay(
+                                            Text("Max Calories")
+                                                .foregroundColor(.white)
+                                                .padding(.vertical, 10)
+                                        )
+                                }
+                            }
+                            .offset (y: -13)
+                            .padding(.horizontal, 20)
+                            
+                            if showHealth {
+                                VStack{
+                                    Text("Minimum Health Score:  \(numberFormatter.string(from: NSNumber(value: score))!)")
+                                        .font(.system(size:20))
+                                    
+                                    Slider(value: $score, in: 0...10, step: 1.0, minimumValueLabel: Text("0"), maximumValueLabel: Text("10"), label: {})
+                                        .padding(.horizontal,20)
+                                        .tint(custGreen)
+                                }
+                                .offset(y:-5)
+                            }
+                            
+                            if showMinCal {
+                                VStack{
+                                    Text("Minimum Calories:  \(numberFormatter.string(from: NSNumber(value: min))!)")
+                                        .font(.system(size:20))
+                                    
+                                    Slider(value: $min, in: 100...2000, step: 10.0, minimumValueLabel: Text("100"), maximumValueLabel: Text("2000"), label: {})
+                                        .padding(.horizontal,20)
+                                        .tint(custMedGreen)
+                                        
+                                }
+                                .offset(y:-5)
+                            }
+                            
+                            if showMaxCal {
+                                VStack{
+                                    Text("Maximum Calories:  \(numberFormatter.string(from: NSNumber(value: max))!)")
+                                        .font(.system(size:20))
+                                    
+                                    Slider(value: $max, in: 100...2000, step: 10.0, minimumValueLabel: Text("100"), maximumValueLabel: Text("2000"), label: {})
+                                        .padding(.horizontal,20)
+                                        .tint(custDarkGreen)
+                                }
+                                .offset(y:-5)
+                            }
+                        }
+            
             
             if (recipesViewModel.loading) {
                 ProgressView()
@@ -56,4 +195,17 @@ struct Recipes: View {
         
     }
 
+}
+
+struct Recipes_Previews: PreviewProvider {
+    static var previews: some View {
+        // Substitute this with your actual ViewModel initialization
+        let viewModel = RecipesViewModel()
+        
+        // Define a mock openCart function
+        let mockOpenCart = {}
+        
+        // Use viewModel and mockOpenCart function in your Recipes view
+        Recipes(recipesViewModel: viewModel, openCart: mockOpenCart)
+    }
 }
