7a09bf9cf8cbb2496ebdfbf1d5e67b0d15658d37
rsrafik <tarvotur15@gmail.com>
2023-06-01 09:19:00 -0500

    organization

--- EasyMeal/Views/ForgotPassword.swift
+++ EasyMeal/Views/ForgotPassword.swift
@@ -1,36 +1,47 @@
-//
-//  ForgotPassword.swift
-//  EasyMeal
-//
-//  Created by Alexander Masztak on 5/22/23.
-//
+// ForgotPassword.swift
+// EasyMeal
+// Created by Alexander Masztak on 5/22/23.
 
 import SwiftUI
 
-struct ForgotPassword: View {
+// View for password reset
+struct ForgotPassword: View
+{
+    
+    // Firebase manager and view model
     @EnvironmentObject var firebaseManager: FirebaseManager
     @ObservedObject var forgotPasswordViewModel: ForgotPasswordViewModel
     
-    init(closeTab: @escaping () -> Void) {
+    // Initializer
+    init(closeTab: @escaping () -> Void)
+    {
         forgotPasswordViewModel = ForgotPasswordViewModel(closeTab: closeTab)
     }
 
-    var body: some View {
-        if forgotPasswordViewModel.passwordResetSent {
+    // View body
+    var body: some View
+    {
+        // Display reset confirmation if reset sent
+        if forgotPasswordViewModel.passwordResetSent
+        {
             VStack {
                 Spacer()
-                HStack {
+                HStack
+                {
                     Spacer()
                     Text("Password reset sent.")
                     Spacer()
                 }
                 Spacer()
             }
-            
-        } else {
-            VStack(alignment: .leading) {
-                
-                HStack {
+        } else
+        {
+            // Otherwise display reset form
+            VStack(alignment: .leading)
+            {
+                // Logo and app name
+                HStack
+                {
                     Image("logo")
                         .resizable()
                         .frame(width: 50, height: 83)
@@ -45,11 +56,9 @@ struct ForgotPassword: View {
                 }
                 .frame(maxWidth: .infinity, alignment: .leading)
                 
-//                Text("Forgot Password")
-//                    .bold()
-//                    .foregroundColor(.black)
-//                    .font(.system(size: 30))
-                HStack {
+                // Email input field
+                HStack
+                {
                     Image(systemName: "envelope")
                         .foregroundColor(Color(hex: "747474"))
                     TextField("Email", text: $forgotPasswordViewModel.email)
@@ -57,15 +66,19 @@ struct ForgotPassword: View {
                         .keyboardType(.emailAddress)
                 }
                 
-                Button(action: {
-                    firebaseManager.resetPassword(email: forgotPasswordViewModel.email) {
+                // Reset password button
+                Button(action:
+                {
+                    // Request password reset on click
+                    firebaseManager.resetPassword(email: forgotPasswordViewModel.email)
+                    {
                         forgotPasswordViewModel.passwordResetSent = true
-                    } onError: {
+                    } onError:
+                    {
                         forgotPasswordViewModel.passwordResetSent = false
                     }
-                    
-                    
-                }) {
+                })
+                {
                     Text("Reset Password")
                         .frame(width: 234, height: 50)
                         .cornerRadius(20)
@@ -76,11 +89,8 @@ struct ForgotPassword: View {
                 .cornerRadius(10)
                 .padding(.top, 20)
                 Spacer()
-                
             }
             .padding(30)
         }
-        
     }
 }
-
--- EasyMeal/Views/RecipeView.swift
+++ EasyMeal/Views/RecipeView.swift
@@ -1,49 +1,32 @@
-//
 //  RecipeView.swift
 //  EasyMeal
-//
 //  Created by Miguel Aenlle on 5/18/23.
-//
 
 import SwiftUI
 
-struct RecipeView: View {
+struct RecipeView: View
+{
     var recipe: Recipe
     var selectRecipe: (Recipe) -> Void
-    var body: some View {
-        VStack(spacing: 8) {
-//            VStack {
-//                AsyncImage(url: URL(string:recipe.url)) { image in
-//                    image
-//                        .resizable()
-//                        .scaledToFill()
-//                        .frame(width: .infinity, height: 150)
-//                        .cornerRadius(10)
-//                } placeholder: {
-//                    ZStack {
-//                        Color(red: 0.8, green: 0.8, blue: 0.8)
-//                            .cornerRadius(10)
-//                        Text("No Image")
-//                            .foregroundColor(.gray)
-//                            .font(.headline)
-//                    }
-//                    .frame(width: .infinity, height: 150)
-//                    
-//                }
-//            }
-//        
 
-            VStack(alignment: .leading) {
+    var body: some View
+    {
+        VStack(spacing: 8)
+        {
+            VStack(alignment: .leading)
+            {
                 Text(recipe.name)
                     .font(.system(size: 18))
                     .fontWeight(.medium)
                     .multilineTextAlignment(.leading)
                     .foregroundColor(.black)
                     .frame(maxWidth: .infinity, alignment: .leading)
+
                 Text("\(recipe.numIngredients)/\(recipe.totalIngredients) Ingredients")
                     .foregroundColor(.gray)
                     .font(.subheadline)
                     .frame(maxWidth: .infinity, alignment: .leading)
+
                 Text("\(String(format: "%.2f", round(recipe.healthScore*10)/10))/10 Health Score")
                     .foregroundColor(.gray)
                     .font(.subheadline)
@@ -52,12 +35,12 @@ struct RecipeView: View {
             .padding(.horizontal, 15)
             .padding(.bottom, 20)
             .padding(.top, 20)
-
         }
         .background(Color.white)
         .cornerRadius(10)
         .shadow(color: Color(hex: "#d4d4d4"), radius: 5,x: 0, y: 5)
-        .onTapGesture {
+        .onTapGesture
+        {
             selectRecipe(recipe)
         }
     }
